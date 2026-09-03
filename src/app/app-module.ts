b184c4c98201fa8ffc08e3792a769113d82211f8
Amane <2525777@cognizant.com>
2026-09-03 10:45:51 +0530

    corrected relatedProduct html code

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,6 +2,7 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ProductModule } from './product/product-module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/product/product-module.ts
+++ src/app/product/product-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDetail } from './product-detail/product-detail';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ProductDetail],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule],
 })
 export class ProductModule {}
