535066bac1fdc3312eee5a7f44b29468c3b35d2e
Amane <2525777@cognizant.com>
2026-09-04 10:12:23 +0530

    added RouterModule in authModule

--- src/app/auth/auth-module.ts
+++ src/app/auth/auth-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
 })
 export class AuthModule {}
